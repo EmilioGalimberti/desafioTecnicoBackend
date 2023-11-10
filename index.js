add17846590548f00310960d413951332598f000
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-11-10 15:45:01 -0300

    +index.js server con express

---
+++ index.js
@@ -0,0 +1,17 @@
+import  express  from "express";
+import cors from "cors";
+//mport router from "./routes/router.js";
+//import { sincronizarDB } from "./service/sincronizacion.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 3000;
+
+//app.use('/',  router)
+
+app.listen(port, async() =>{
+    console.log("Inicio de api puerto ",port)
+    //sincronizarDB.start()
+})
