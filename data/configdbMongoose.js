62b7f878a908cb629ca8efc1235e337e1a4b90c2
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-11-10 15:46:03 -0300

    +./data/configdbMongoose.js uso de mongoose conexion db

---
+++ data/configdbMongoose.js
@@ -0,0 +1,19 @@
+
+import mongoose from 'mongoose';
+
+const url = 'mongodb+srv://emiliogalimberty:DykCvZgFNarwXjFG@starwarsclusterapi.xhvj1ul.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.error('Error de conexión a la base de datos:', error);
+});
+db.once('open', () => {
+  console.log('Conexión exitosa a la base de datos en la nube');
+});
+
+export default mongoose; 
